fe940d7d81d63f46d138c60c37b6856969dbe6ab
sagebeme <eudicas5k@gmail.com>
2023-02-28 19:52:29 +0300

    creating a new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,12 +10,6 @@
  * FUNCTIONALITY
  *
  * 1. Allocate memory for a new binary tree node.
- * 2. If the allocation fails, return NULL.
- * 3. Set the value of the new node to the value passed in.
- * 4. Set the parent of the new node to the parent passed in.
- * 5. Set the left child of the new node to NULL.
- * 6. Set the right child of the new node to NULL.
- * 7. Return a pointer to the new node.
  *
  * Time Complexity: O(1)
  */
@@ -32,3 +26,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new->right = NULL;
 
 	return (new);
+}
