4ac3409a75d73692103cf3f47d0c8c50d3456777
sagebeme <eudicas5k@gmail.com>
2023-03-02 17:07:32 +0300

    finding binary tree height;git push

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - the depth of the binary tree
+ *
+ * @tree: root node of the tree to get the depth
+ *
+ * Return: 0
+ *
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent):0);
+}
