d5e5712d19a8cb8d0da75db6368fdd8e93f356bc
sagebeme <eudicas5k@gmail.com>
2023-03-02 19:15:11 +0300

    adding prototypes and header file

--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,8 +24,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+/* Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
+/*AVL TREE*/
 typedef struct binary_tree_s avl_t;
+/* Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
 
 /**
