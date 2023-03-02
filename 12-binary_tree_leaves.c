3fac3edc82a2c410fbb6d588635b2a8aa8900522
sagebeme <eudicas5k@gmail.com>
2023-03-02 17:20:59 +0300

    finding the leaves of a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_leaves - finds leaves in the tree
+ * @tree: the root node of the tree
+ *
+ * Return: number of leaves in tree
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+}
